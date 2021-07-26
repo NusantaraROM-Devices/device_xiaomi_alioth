29b3edef16edd34b87c52e6f1731d0017a17b604
goodmeow <harunbam3@gmail.com>
2021-07-26 13:04:30 +0700

    alioth: initial Nusantara config

--- nad_alioth.mk
+++ nad_alioth.mk
@@ -8,21 +8,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit some common NusantaraProject stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+TARGET_USES_BLUR := true
+USE_PIXEL_CHARGING := true
+NAD_BUILD_TYPE ?= UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+USE_GAPPS ?= true
+USE_AOSP_CLOCK := true
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := nad_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="alioth-user 11 RKQ1.200826.002 V12.5.2.0.RKHMIXM release-keys"
-
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:11/RKQ1.200826.002/V12.5.2.0.RKHMIXM:user/release-keys
