bde4f34d9438d5e5d2b4ff5c961e6ca683f76f69
SahilSonar <sss.sonar2003@gmail.com>
2021-07-30 06:18:54 +0200

    alioth: overlay-lineage: Set proper fingerprint sensor location
    Change-Id: I5870100b1df2004f0541bba22f7a355234d082c6

--- device.mk
+++ device.mk
@@ -42,7 +42,8 @@ PRODUCT_VIRTUAL_AB_OTA := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
