880520acd92be12733438a051224b054e51b0332
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-10 01:01:53 +0100

    alioth: Update blobs from V12.5.4.0.RKHMIXM
    * Also move to muppets based vendor
    
    Change-Id: I52f47ea15140af63ba9f0b0a7b9d237a25abdbdb

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -9,20 +9,8 @@
 
 #include "vendor_init.h"
 
-#define FINGERPRINT "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys"
-
-static const variant_info_t aliothcn_info = {
-    .hwc_value = "CN",
-    .sku_value = "",
-
-    .brand = "Redmi",
-    .device = "alioth",
-    .marketname = "K40",
-    .model = "M2012K11AC",
-    .build_fingerprint = FINGERPRINT,
-
-    .nfc = true,
-};
+#define DESCRIPTION "alioth_global-user 11 RKQ1.200826.002 V12.5.4.0.RKHMIXM release-keys"
+#define FINGERPRINT "google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys"
 
 static const variant_info_t aliothin_info = {
     .hwc_value = "INDIA",
@@ -32,12 +20,13 @@ static const variant_info_t aliothin_info = {
     .device = "aliothin",
     .marketname = "Mi 11X",
     .model = "M2012K11AI",
+    .build_description = DESCRIPTION,
     .build_fingerprint = FINGERPRINT,
 
     .nfc = false,
 };
 
-static const variant_info_t alioth_info = {
+static const variant_info_t alioth_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
 
@@ -45,14 +34,29 @@ static const variant_info_t alioth_info = {
     .device = "alioth",
     .marketname = "POCO F3",
     .model = "M2012K11AG",
+    .build_description = DESCRIPTION,
+    .build_fingerprint = FINGERPRINT,
+
+    .nfc = true,
+};
+
+static const variant_info_t alioth_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "alioth",
+    .marketname = "K40",
+    .model = "M2012K11AC",
+    .build_description = DESCRIPTION,
     .build_fingerprint = FINGERPRINT,
 
     .nfc = true,
 };
 
 static const std::vector<variant_info_t> variants = {
-    aliothcn_info,
     aliothin_info,
+    alioth_global_info,
     alioth_info,
 };
 
